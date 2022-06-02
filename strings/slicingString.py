587e93c0092a5d9f7ab8bed7e58b0a8c4d402147
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-02 07:34:22 +0100

    slice string

---
+++ strings/slicingString.py
@@ -0,0 +1,17 @@
+text = "Hello Black~vibes"
+
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+# print(text[1:5])
+
+# slicing from the start
+# By leaving out the start index, the range will start at the first character
+# print(text[:5])
+
+# slice to the end
+# By leaving out the end index, the range will go to the end
+# print(text[6:])
+
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string
+# print(text[-11:-6])
+# print(text[:-2])
---
+++ strings/strings.py
@@ -0,0 +1,23 @@
+greeting = "Hello Black~vibes"
+
+# lopping through a string
+# for x in greeting:
+#     print(x)
+
+# characters from a string can be accessed with a postion like and array
+# print(greeting[1])
+
+# getting the length of a string
+# print(len(greeting))
+
+# To check if a certain phrase or character is present in a string, we can use the keyword in.
+# print("Hello" in greeting) #true
+# print("Hi" in greeting) #false
+# print("H" in greeting) #true
+# print("h" in greeting) #false
+
+# checking using if statement
+# if "Hello" in greeting:
+#   print("Yes, Hello is present in greeting")
+# if "Hi" not in greeting:
+#   print("Hi is not in greeting")
