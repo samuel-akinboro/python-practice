9ee0da6653865af5cc6559525a86c162fdd5bf6a
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-02 07:46:19 +0100

    modify string

---
+++ strings/modifyString.py
@@ -0,0 +1,17 @@
+text = "Hello Black~vibes"
+
+# upper case
+print(text.upper())
+
+# lower case
+print(text.lower())
+
+# remove whitespace
+sample = "  Hello world  "
+print(sample.strip())
+
+# replace string
+print(text.replace("Hello", "Welcome"))
+
+# split string
+print(text.split(" "))
