230ea3b39c6270a9a21799153e9e8a6a9267038d
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-03 09:31:44 +0100

    boolean

---
+++ boolean/bool.py
@@ -0,0 +1,14 @@
+x = 2
+y = 5
+
+if x > y :
+  print("Yeah, x is greater than y")
+else :
+  print("x is less than y")
+
+# The bool() function allows you to evaluate any value, and give you True or False in return
+print(bool(False))
+
+# isinstance() function can be used to determine if an object is of a certain data type
+x = True
+print(isinstance(x, int))
