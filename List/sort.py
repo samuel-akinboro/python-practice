165150a5d818d4fb9f86efafdbbeeba62aa82310
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-03 16:44:54 +0100

    sort list

---
+++ List/sort.py
@@ -0,0 +1,11 @@
+thisList = ["banana", "apple", "grape", "eggplant"]
+thisList.sort()
+print(thisList)
+
+# descending order
+thisList.sort(reverse = True)
+print(thisList)
+
+# The reverse() method reverses the current sorting order of the elements.
+thisList.reverse()
+print(thisList)
