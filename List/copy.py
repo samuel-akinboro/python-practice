8539284d1654c21e334605f0593cd2865d61ecca
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-03 16:52:37 +0100

    copy list

---
+++ List/copy.py
@@ -0,0 +1,9 @@
+# you can make copies of a list by using the copy() method
+thisList = ["apple", "orange"]
+newList = thisList.copy()
+print(newList)
+
+# you can also use list()
+fruits = ["pear", "grape"]
+newList2 = list(fruits)
+print(newList2)
