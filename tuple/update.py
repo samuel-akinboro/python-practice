226ca7d77c52ba4e29a0d438cb9eba7c5b89d508
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-04 21:56:02 +0100

    update tuple

---
+++ tuple/update.py
@@ -0,0 +1,9 @@
+# you can't edit a tuple
+# but you can convert the tuple tp a list, edit it and change it back to a tuple
+
+x = ("book", "ruler")
+y = list(x)
+y[0] = "math set"
+x = tuple(y)
+
+print(x)
