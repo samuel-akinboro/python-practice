02d7da5087065cdf63794fa199c4acf43a58f39f
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-06-07 11:34:47 +0100

    update a dictionary

---
+++ dictionary/change.py
@@ -0,0 +1,12 @@
+thisDict = {
+  "name": "Black~vibes",
+  "title": "Frontend Engineer",
+  "stack": ["html", "css", "javascript", "React", "React Native"]
+}
+
+thisDict["title"] = "Senior Frontend Engineer"
+print(thisDict)
+
+# you can also use the update method to make changes to the dictionary
+thisDict.update({"stack": ["html", "css", "tailwind css", "javascript", "React", "React Native"], "name": "Black~vibes~codes"})
+print(thisDict)
